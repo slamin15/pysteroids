ab742e6845494ddbcb770629d77d78229863d9e0
teets <tietemasam@gmail.com>
2025-05-22 20:07:34 -0400

    figureing things out

---
+++ asteroid.py
@@ -0,0 +1,15 @@
+import pygame
+from circleshape import CircleShape
+
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "red", self.position, self.radius, 2)
+
+    def update(self, dt):
+        x = self.position[0]
+        y = self.position[1]
+        self.position = (x + dt, y + dt)
--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updateable = pygame.sprite.Group(player)
+    drawable = pygame.sprite.Group(player)
+    Player.containers = (updateable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         
+        updateable.update(dt)
+
         screen.fill("black")
+        
+        for obj in drawable:
+            obj.draw(screen)
+
+
 
         pygame.display.flip()
+
+        dt = clock.tick(60)
         
 
 if __name__ == "__main__":
